e97f8e2be6dcff00d8afe9b0db9534cbf18edc72
Seyi Onifade <xyluz@ymail.com>
2018-12-20 02:35:04 -0800

    layout

---
+++ resources/views/auth.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="./assets/logo.svg">
+  <meta name="description" content="Governance Clout is a platform for nigerians to give their takes and opinions on their government and how they are governed. It is open for all types of users. GClout gives your direct access to the government officials that you have elected">
+  <meta name="keywords" content="Government, Nigeria, Opinion, GClout">
+  <meta name="author" content="staybusy.ng">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#00B0F0">
+  <meta name="og:title" property="og:title" content="Governance Clout | Coming soon">
+  <meta name="og:description" property="og:title" content="Governance Clout is a social networking site for everyone (starting from Nigerians) to socialize, contribute and give opinions on governance at all levels. It is open for all type of users. GClout gives you direct access to the government officials that you have elected.">
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"> -->
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+  <link rel="stylesheet" href="./assets/style.css">
+  <title>GClout | Admin - Login</title>
+</head>
+
+<body>
+	<nav class="g-navbar">
+        <div class="container">
+            <img src="./assets/logo.svg" alt="gclout branding">
+        </div>
+    </nav>
+    <section class="d-flex auth-wrapper">
+        <div class="m-auto col-md-5">
+            <div class="auth-header text-center py-3">
+                <h2>Sign In</h2>
+            </div>
+            <div class="auth-form">
+                <form>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <div class="input-group">
+                            <input type="email" class="form-control" required placeholder="Email address">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="material-icons">email</i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control" required placeholder="Password">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="material-icons">lock</i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-block btn-gclout-blue">Sign In</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="./assets/main.js"></script>
+
+</html>
