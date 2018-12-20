964cbc5efb2ffe3875baf7a74d782c1505c82a67
Seyi Onifade <xyluz@ymail.com>
2018-12-20 02:47:22 -0800

    asset helper function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Admin User Things, authentication, add new, etc
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
